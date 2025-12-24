cdb624272ed153b26fe49cd04e6368d1f43d4cb2
Abhinay Jangde <abhinayjangde@gmail.com>
2025-12-24 11:09:12 +0530

    add authorization header

--- web/src/clients/api.ts
+++ web/src/clients/api.ts
@@ -1,6 +1,8 @@
 import { GraphQLClient } from "graphql-request"
 
 export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string, {
-    headers: {},
+    headers: {
+        "Authorization": `Bearer ${typeof window !== "undefined" ? localStorage.getItem("spread_token") : ""}`
+    },
 })
 
